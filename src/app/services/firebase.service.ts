d53a8878650197f79b532a5abfc1af7ae9aabfc4
EmmanuelChavezNataren <emmanuel.chavez.condor@gmail.com>
2024-05-06 18:06:09 -0600

    chore: add sign-out event

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,15 +2,17 @@ import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getAuth, updateProfile } from 'firebase/auth';
+import { UtilsService } from 'src/app/services/utils.service';
 import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
+  //Injects
   #authFire = inject(AngularFireAuth);
   #fireDB = inject(AngularFirestore);
-
+  #utilsServ = inject(UtilsService);
   constructor() {}
 
   /** AUTH **/
@@ -29,5 +31,15 @@ export class FirebaseService {
   updateUser(user: any) {
     const auth = getAuth();
     return updateProfile(auth.currentUser, user);
+  }
+
+  getAuthState() {
+    return this.#authFire.authState;
+  }
+
+  async signOut() {
+    await this.#authFire.signOut();
+    this.#utilsServ.routerLink('/auth');
+    localStorage.removeItem('user');
   }
 }
